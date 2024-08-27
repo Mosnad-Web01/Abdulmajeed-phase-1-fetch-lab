ec03d120ff24395498e8c9edec6b3fd7bf2c6611
Abdulmajeed Alselmi <hh738hhh@gmail.com>
2024-08-27 15:03:42 +0300

    use fetch

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(data => renderBooks(data))
+  .catch((error) => console.error('Error:', error));
+  
+
+
   // To pass the tests, don't forget to return your fetch!
   
 }
